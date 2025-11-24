75290e44167f120c27a329623264de25c1e8cc10
Andrew Davis <andrew.davis@habb.tech>
2025-11-24 08:11:27 -0800

    Update settings.gradle.kts
    Fixed naming issue
    
    Signed-off-by: Andrew Davis <andrew.davis@habb.tech>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,6 +15,23 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+/*
+ * Copyright (c) 2025 Meshtastic LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 include(
     ":app",
     ":core:analytics",
@@ -45,7 +62,6 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     includeBuild("build-logic")
-    
     repositories {
         google()
         mavenCentral()
@@ -67,7 +83,6 @@ dependencyResolutionManagement {
 plugins {
     id("org.gradle.toolchains.foojay-resolver") version "1.0.0"
     id("com.gradle.develocity") version("4.2.2")
-   
     id("com.gradle.common-custom-user-data-gradle-plugin") version "2.4.0"
 }
 
@@ -86,7 +101,6 @@ toolchainManagement {
         javaRepositories {
             repository("foojay") {
                 resolverClass.set(org.gradle.toolchains.foojay.FoojayToolchainResolver::class.java)
-   
             }
         }
     }
